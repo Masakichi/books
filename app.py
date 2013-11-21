a3d7c43b1572fbff6e4329ba773ce96d3ef604a2
gimo <self@gimo.me>
2013-11-21 15:42:57 +0800

    add requirements.txt

--- app.py
+++ app.py
@@ -4,4 +4,21 @@
 from settings import app
 
 #views
+@app.route('/')
+def index():
+    pass
+
+@app.route('/book/<int:book_id>')
+def book():
+    pass
+
+@app.route('/top')
+def top():
+    pass
+
+@app.route('/tags')
+def tags():
+    pass
+
+#run
 app.run()
