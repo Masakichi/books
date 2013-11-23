b21d3aa6423946f55a6bae5eaec547673e9e152b
gimo <self@gimo.me>
2013-11-23 22:13:33 +0800

    modify book model and creat user model.

--- models/book.py
+++ models/book.py
@@ -6,21 +6,37 @@ from books import db
 
 
 class Book(db.Document):
-    book_id = db.StringField(required=True)
-    book_title = db.StringField(required=True)
-    book_author =
-    book_translator =
-    book_img =
-    book_url =
-    book_summary =
-    book_publisher =
-    book_isbn =
-    book_tags = ListField()
-    book_add_time = db.DateTimeField(default=datetime.datetime.now, required=True)
+    bid = db.StringField(required=True)
+    title = db.StringField(required=True)
+    author = db.ListField()
+    translator = db.ListField()
+    img = db.URLField()
+    url = db.URLField()
+    summary = db.StringField()
+    publisher = db.StringField()
+    isbn = db.StringField()
+    tags = db.ListField(db.ReferenceField('Tag'))
+    add_time = db.DateTimeField(default=datetime.datetime.now, required=True)
+    raters_num = db.IntField()
+    rating_avg = db.StringField()
 
-class Tag(db.EmbeddedDocument):
-    tag_name =
-    tag_title =
-    tag_count =
+    def __unicode__(self):
+        return self.title
 
-class Resource(db.EmbeddedDocument):
+
+class Tag(db.Document):
+    name = db.StringField()
+    count = db.IntField()
+
+    def __unicode__(self):
+        return self.name
+
+
+#Class Author(db.Document):
+
+#class Resource(db.EmbeddedDocument):
+    #resource_link =
+
+
+#class Comment(db.EmbeddedDocumentj):
+    #pass
---
+++ models/user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from books import db
+import Book
+
+class User(db.Document):
+    user_name =
+    user_email =
+    user_push_email =
+    user_type =
+    user_type =
+
